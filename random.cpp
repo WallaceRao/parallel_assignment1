257de7c1dca9a05eb54e1911b22305540b986674
Yonghui <raoyonghui0630@gmail.com>
2017-07-25 16:09:35 +1200

    the second commit

--- random.cpp
+++ random.cpp
@@ -1,10 +1,8 @@
 
-//first.cpp Adding numbers using two nodes C++ version
+//random.cpp Calculate PI
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <iostream>
-
 #include "mpi.h"
 
 //always use argc and argv, as mpirun will pass the appropriate parms.
@@ -16,19 +14,15 @@ int main(int argc,char* argv[])
   int myid = MPI::COMM_WORLD.Get_rank();
   int numproc = MPI::COMM_WORLD.Get_size();
   std::cout << "This is id " << myid << " out of " << numproc << std::endl;
-
-  
-
-
   if (myid == 0) { // master
 
   long a = 1664525;
   long m = 1;
   int pow = 32;
   long c = 1013904223;
-  long n_pre = 12345;
+  long n_seed = 12345;
   long *n_start = new long[numproc];
-  *n_start = n_pre;
+  *n_start = n_seed;
   
   //calculate m
   for (int i = 0; i < pow; i ++)
@@ -43,7 +37,7 @@ int main(int argc,char* argv[])
   for(int i = 0; i < k; i ++)
     {
         ca = ca * a % m;
-        long caim = 1 * c;  // c * pow(a,i) mod m
+        long caim = 1 * c;
         for(int j = 0; j < i; j ++)
         {
             caim = (caim * a) % m;
@@ -58,7 +52,7 @@ int main(int argc,char* argv[])
   {
     long ni_pre =  *(n_start + i - 1);
     *(n_start + i) = (ca * ni_pre % m + cc % m) % m;  // x(i+k) = (Ax(i) + C) mod m
-    // Master sends 'n_start' to slaves
+    // Master sends 'n_start', k, a, c, m to slaves
      MPI::COMM_WORLD.Send(n_start + i, 1, MPI::LONG, i,0);
      MPI::COMM_WORLD.Send(k, 1, MPI::LONG, i,0);
      MPI::COMM_WORLD.Send(a, 1, MPI::LONG, i,0);
@@ -67,34 +61,69 @@ int main(int argc,char* argv[])
   }
   
     // Partial result for node 0
-    int sum0 = 0;
+    long sum = 0; // sum of random intergers that locate in the circle
+    // process the first random
+    long sqrtm = sqrt(m);
+    float xi = (*n_start / sqrtm -  sqrtm/2.0) / (sqrtm/2.0);
+    float yi = (*n_start % sqrtm -  sqrtm/2.0) / (sqrtm/2.0);
+    if((xi*xi + yi*yi) <= 1)
+        sum++;
+    long n_pre = *n_start;
+    // process the folloing randoms
     for(int i = 1; i <= k; i++){
-      //n i+1 = (an i + c) mod m
-      
-      
-      sum0 = sum0 + i;
+      //n(i+1) = (an(i) + c) mod m
+      long n = (a * n_pre + c) % m;
+      float xi = (n / sqrtm -  sqrtm/2.0) / (sqrtm/2.0);
+      float yi = (n % sqrtm -  sqrtm/2.0) / (sqrtm/2.0);
+      if((xi*xi + yi*yi) <= 1)
+        sum++;
+      n_pre = n;
     }
 
-    //Master waits to receive 'sum1' from slave
-    int sum1;
-    MPI::COMM_WORLD.Recv(&sum1, 1, MPI::INT, 1,0);
-    int result = sum0 + sum1;
+    //Master waits to receive 'sum' from slaves
+    
+    for(int i = 1; i < numproc; i ++)
+    {
+        int slaveSum;
+        MPI::COMM_WORLD.Recv(&slaveSum, 1, MPI::LONG, i, 0);
+        sum += slaveSum;
+    }
+        
+    float PI = 4.0 * sum / m;
 
-    std::cout << "The final result is " << result << std::endl;
+    std::cout << "The final result is " << PI << std::endl;
   }
 
   else {  // slave
 
     // Slave waits to receive 'N' from master
-    int N;
-    MPI::COMM_WORLD.Recv(&N, 1, MPI::INT, 0, 0);
-    int sum1 = 0;
-    for(int i = N/2 + 1; i <= N; i++){
-      sum1 = sum1 + i;
+    long n_start;
+    long k, a, c, m;
+    MPI::COMM_WORLD.Recv(&n_start, 1, MPI::LONG, 0, 0);
+    MPI::COMM_WORLD.Recv(&k, 1, MPI::LONG, 0, 0);
+    MPI::COMM_WORLD.Recv(&a, 1, MPI::LONG, 0, 0);
+    MPI::COMM_WORLD.Recv(&c, 1, MPI::LONG, 0, 0);
+    MPI::COMM_WORLD.Recv(&m, 1, MPI::LONG, 0, 0);
+    long slaveSum = 0; // sum of random intergers that locate in the circle
+    // process the first random
+    long sqrtm = sqrt(m);
+    float xi = (n_start / sqrtm -  sqrtm/2.0) / (sqrtm/2.0);
+    float yi = (n_start % sqrtm -  sqrtm/2.0) / (sqrtm/2.0);
+    if((xi*xi + yi*yi) <= 1)
+        slaveSum++;
+    long n_pre = n_start;
+    // process the folloing randoms
+    for(int i = 1; i <= k; i++){
+      //n(i+1) = (an(i) + c) mod m
+      long n = (a * n_pre + c) % m;
+      float xi = (n / sqrtm -  sqrtm/2.0) / (sqrtm/2.0);
+      float yi = (n % sqrtm -  sqrtm/2.0) / (sqrtm/2.0);
+      if((xi*xi + yi*yi) <= 1)
+        slaveSum++;
+      n_pre = n;
     }
-
-    // Slave sends 'sum1' to master
-    MPI::COMM_WORLD.Send(&sum1, 1, MPI_INT, 0, 0);
+    // Slave sends 'slaveSum' to master
+    MPI::COMM_WORLD.Send(&slaveSum, 1, MPI_INT, 0, 0);
   }
   MPI::Finalize();
 }
